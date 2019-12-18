8f5b175d5290847219a2441a54591f9db173be1b
willrmachado <willian.rodrigues.machado@hotmail.com>
2019-12-18 05:17:25 -0200

    Publish to netlify production

---
+++ build/precache-manifest.e7b444f4caadba78243ae9744c2b6edb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9a38a1b05cb2dfb0c0026fbfaaec40",
+    "url": "/WillRMachado/spotify-app/index.html"
+  },
+  {
+    "revision": "63417a953d1b08ee525d",
+    "url": "/WillRMachado/spotify-app/static/css/main.453eabda.chunk.css"
+  },
+  {
+    "revision": "2f95d0279ab2c84be4ce",
+    "url": "/WillRMachado/spotify-app/static/js/2.702ddaaa.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/WillRMachado/spotify-app/static/js/2.702ddaaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63417a953d1b08ee525d",
+    "url": "/WillRMachado/spotify-app/static/js/main.bdd173a0.chunk.js"
+  },
+  {
+    "revision": "032a62253c54408aa611",
+    "url": "/WillRMachado/spotify-app/static/js/runtime-main.f7c72608.js"
+  },
+  {
+    "revision": "cc4e2784f47909e55d940c014cbef882",
+    "url": "/WillRMachado/spotify-app/static/media/Spotify_Icon_RGB_White.cc4e2784.png"
+  }
+]);
